a69e4ea9215b2f30a66f229f0d6bfe5e4d16e4c8
Naren <naren@narendasan.com>
2015-10-27 21:15:37 -0500

    post handler

--- proxy/helpers.go
+++ proxy/helpers.go
@@ -1,5 +1,10 @@
 package proxy
 
+import (
+	"encoding/json"
+	"errors"
+)
+
 func contains(a string, list []string) bool {
 	for _, b := range list {
 		if b == a {
@@ -7,4 +12,41 @@ func contains(a string, list []string) bool {
 		}
 	}
 	return false
+}
+
+func extract(a interface{}, b string) (val string, err error) {
+
+	j, err := json.Marshal(a)
+	if err != nil {
+		return "", errors.New("key does not exist")
+	}
+	// a map container to decode the JSON structure into
+	c := make(map[string]interface{})
+
+	// unmarschal JSON
+	e := json.Unmarshal(j, &c)
+
+	// panic on error
+	if e != nil {
+		panic(e)
+	}
+
+	// a string slice to hold the keys
+	k := make([]string, len(c))
+
+	// iteration counter
+	i := 0
+
+	// copy c's keys into k
+	for s, _ := range c {
+		k[i] = s
+		i++
+	}
+
+	if contains(b, k) {
+		return c[b].(string), nil
+	} else {
+		return "", errors.New("key does not exist")
+	}
+
 }
--- proxy/proxy.go
+++ proxy/proxy.go
@@ -1,9 +1,12 @@
 package proxy
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 )
 
@@ -11,38 +14,110 @@ var JSONHeader string = "application/json; charset=UTF-8"
 var TEXTHeader string = "text/plain; charset=utf-8"
 
 func GETHandler(w http.ResponseWriter, url string, r *http.Request) {
-	response, err := http.Get(url)
+	res, err := http.Get(url)
 	if err != nil {
-		panic(err)
+		InvalidGET(w, err)
+		log.Println(err)
+		return
 	}
+
+	defer res.Body.Close()
+	contents, err := ioutil.ReadAll(res.Body)
+
 	switch {
 
-	case contains(TEXTHeader, response.Header["Content-Type"]):
-		defer response.Body.Close()
-		contents, err := ioutil.ReadAll(response.Body)
+	case contains(TEXTHeader, res.Header["Content-Type"]):
 		if err != nil {
-			panic(err)
+			InvalidGET(w, err)
+			log.Println(err)
+			return
 		}
 		fmt.Fprintf(w, "%s\n", string(contents))
 		break
 
-	case contains(JSONHeader, response.Header["Content-Type"]):
-		defer response.Body.Close()
-		contents, err := ioutil.ReadAll(response.Body)
-
+	case contains(JSONHeader, res.Header["Content-Type"]):
 		var data interface{}
 		err = json.Unmarshal(contents, &data)
 		if err != nil {
-			panic(err)
+			InvalidGET(w, err)
+			log.Println(err)
+			return
 		}
-
-		fmt.Println(data)
 
 		w.Header().Set("Content-Type", JSONHeader)
 		w.WriteHeader(http.StatusOK)
 		if err := json.NewEncoder(w).Encode(data); err != nil {
-			panic(err)
+			InvalidGET(w, err)
+			log.Println(err)
+			return
 		}
 		break
+	}
+}
+
+func POSTHandler(w http.ResponseWriter, url string, r *http.Request) {
+
+	content, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+	if err != nil {
+		InvalidPOST(w, err)
+		log.Println(err)
+		return
+	}
+	if err := r.Body.Close(); err != nil {
+		InvalidPOST(w, err)
+		log.Println(err)
+		return
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(content))
+	req.Header.Set("Content-Type", JSONHeader)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		InvalidPOST(w, err)
+		log.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	contents, err := ioutil.ReadAll(resp.Body)
+	var data interface{}
+	err = json.Unmarshal(contents, &data)
+	if err != nil {
+		InvalidPOST(w, err)
+		log.Println(err)
+		return
+	}
+
+	if resp.StatusCode != http.StatusCreated {
+		InvalidPOST(w, err)
+		log.Println(err)
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		InvalidGET(w, err)
+		log.Println(err)
+		return
+	}
+	w.Header().Set("Content-Type", JSONHeader)
+	w.WriteHeader(http.StatusCreated)
+}
+
+func InvalidPOST(w http.ResponseWriter, err error) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(422) // unprocessable entity
+	data := map[string]interface{}{"Code": 422, "Text": "Unprocessable Entity", "Specfically": err}
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		panic(err)
+	}
+}
+func InvalidGET(w http.ResponseWriter, err error) {
+	// If we didn't find it, 404
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusNotFound)
+	data := map[string]interface{}{"Code": http.StatusNotFound, "Text": "Not Found", "Specfically": err}
+	if err := json.NewEncoder(w).Encode(data); err != nil {
+		panic(err)
 	}
 }
