217b5cbf0b640c3c19a8c31b11b14b23bf314282
Naren <sivagna2@illinois.edu>
2016-03-01 22:45:09 -0600

    DELETE

--- proxy/DELETE.go
+++ proxy/DELETE.go
@@ -2,93 +2,29 @@ package proxy
 
 import (
 	"encoding/json"
-	"encoding/xml"
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func DELETE(w http.ResponseWriter, url string, r *http.Request) {
 	req, err := http.NewRequest("DELETE", url, nil)
-	if err != nil {
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	if resp.StatusCode != http.StatusOK {
 		InvalidDELETE(w, err)
-		log.Println(err)
-		return
-	}
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		InvalidDELETE(w, err)
-		log.Println(err)
+		log.Printf("Failed server %v", err)
 		return
 	}
 
-	defer res.Body.Close()
-	contents, err := ioutil.ReadAll(res.Body)
-
-	switch {
-
-	case contains(JSONHeader, res.Header["Content-Type"]):
-		var data interface{}
-		err = json.Unmarshal(contents, &data)
-		if err != nil {
-			InvalidDELETE(w, err)
-			log.Println(err)
-			return
-		}
-
-		w.Header().Set("Content-Type", JSONHeader)
-		w.WriteHeader(http.StatusOK)
-		if err := json.NewEncoder(w).Encode(data); err != nil {
-			InvalidDELETE(w, err)
-			log.Println(err)
-			return
-		}
-		break
-	case contains(TEXTHeader, res.Header["Content-Type"]):
-		if err != nil {
-			InvalidDELETE(w, err)
-			log.Println(err)
-			return
-		}
-
-		fmt.Fprintf(w, "%s\n", string(contents))
-		break
-
-	case contains(XMLHeader, res.Header["Content-Type"]):
-		var data interface{}
-		err = xml.Unmarshal(contents, &data)
-		if err != nil {
-			InvalidDELETE(w, err)
-			log.Println(err)
-			return
-		}
-
-		w.Header().Set("Content-Type", XMLHeader)
-		w.WriteHeader(http.StatusOK)
-		if err := xml.NewEncoder(w).Encode(data); err != nil {
-			InvalidDELETE(w, err)
-			log.Println(err)
-			return
-		}
-		break
-	default:
-		if err != nil {
-			InvalidDELETE(w, err)
-			log.Println(err)
-			return
-		}
-
-		fmt.Fprintf(w, "%s\n", string(contents))
-		break
-
-	}
+	w.WriteHeader(http.StatusOK)
 }
 
 func InvalidDELETE(w http.ResponseWriter, err error) {
+	// If we didn't find it, 404
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(422) // unprocessable entity
-	data := map[string]interface{}{"Code": 422, "Text": "Unprocessable Entity", "Specfically": err}
+	w.WriteHeader(http.StatusNotFound)
+	data := map[string]interface{}{"Code": http.StatusNotFound, "Text": "Not Found", "Specfically": err}
 	if err := json.NewEncoder(w).Encode(data); err != nil {
 		panic(err)
 	}
