256c79910bb1ea604dcfeef92f56c6e24c1f4ea6
Naren <naren@narendasan.com>
2016-11-12 15:25:29 -0600

    working groups

--- services/groups.go
+++ services/groups.go
@@ -14,14 +14,44 @@ const GroupsFormat string = "JSON"
 //API Interface
 var GroupsRoutes = RouteCollection {
 	Route{
+		"GetGroupTypes",
+		"GET",
+		"/groups",
+		GetGroupsTypes,
+	},
+	Route{
 		"GetGroups",
 		"GET",
-		"/items/{id}",
+		"/groups/:groupType",
 		GetGroups,
+	},
+	Route{
+		"GetGroup",
+		"GET",
+		"/groups/{groupType}/{groupName}",
+		GetGroup,
+	},
+	Route{
+		"IsMember",
+		"GET",
+		"groups/{groupType}/{groupName}?isMember={netid}",
+		IsMember,
 	},
 }
 
 //Route handler
+func GetGroupTypes(w http.ResponseWriter, r *http.Request) {
+	proxy.GET(w, AuthURL+r.URL.String(), AuthFormat, "", r)
+}
+
 func GetGroups(w http.ResponseWriter, r *http.Request) {
+	proxy.GET(w, AuthURL+r.URL.String(), AuthFormat, "", r)
+}
+
+func GetGroup(w http.ResponseWriter, r *http.Request) {
+	proxy.GET(w, AuthURL+r.URL.String(), AuthFormat, "", r)
+}
+
+func IsMember(w http.ResponseWriter, r *http.Request) {
 	proxy.GET(w, AuthURL+r.URL.String(), AuthFormat, "", r)
 }
