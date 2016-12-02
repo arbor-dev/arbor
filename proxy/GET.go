d790109ff7ff580c09f2d8311bf08a7587bf24e1
Naren Dasan <naren@narendasan.com>
2016-12-02 14:34:08 -0600

    adding NCSA License

--- proxy/GET.go
+++ proxy/GET.go
@@ -1,3 +1,13 @@
+/**
+* Copyright © 2016, ACM@UIUC
+*
+* This file is part of the Groot Project.  
+* 
+* The Groot Project is open source software, released under the University of
+* Illinois/NCSA Open Source License. You should have received a copy of
+* this license in a file with the distribution.
+**/
+
 package proxy
 
 import (
