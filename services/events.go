d8476577b611878cc7d633b2d99bb56c28bce6d4
Sameet Sapra <sameetandpotatoes@gmail.com>
2017-01-10 12:38:28 -0600

    Add upcoming events

---
+++ services/events.go
@@ -0,0 +1,47 @@
+/**
+* Copyright © 2016, ACM@UIUC
+*
+* This file is part of the Groot Project.  
+* 
+* The Groot Project is open source software, released under the University of
+* Illinois/NCSA Open Source License. You should have received a copy of
+* this license in a file with the distribution.
+**/
+
+package services
+
+import (
+	"net/http"
+	"github.com/acm-uiuc/groot/proxy"
+)
+
+//Location
+const EventsURL string = "http://localhost:8002"
+
+//Service Data Type
+const EventsFormat string = "JSON"
+
+//API Interface
+var EventsRoutes = RouteCollection {
+	Route{
+		"GetEvents",
+		"GET",
+		"/events",
+		GetEvents,
+	},
+	Route{
+		"GetUpcomingEvents",
+		"GET",
+		"/events/upcoming",
+		GetUpcomingEvents,
+	},
+}
+
+//Route handler
+func GetEvents(w http.ResponseWriter, r *http.Request) {
+	proxy.GET(w, EventsURL+r.URL.String(), EventsFormat, "", r)
+}
+
+func GetUpcomingEvents(w http.ResponseWriter, r *http.Request) {
+	proxy.GET(w, EventsURL+r.URL.String(), EventsFormat, "", r)
+}
