d137e7a99a83e89740b02add1f697fdb13c10b43
Arnav Sankaran <arnavsankaran@gmail.com>
2019-01-05 20:07:02 -0600

    Refactor out init() from json middlewares

--- proxy/json.go
+++ proxy/json.go
@@ -9,46 +9,39 @@ import (
 )
 
 // A handler for writing errors into the response sent to the caller
-var JsonErrorHandler http.Handler
+var JsonErrorHandler = http.HandlerFunc(func(w http.ResponseWriter, r* http.Request) {
+	w.WriteHeader(500)
+})
+
+// A handler which validates the request body for valid json
+var jsonValidator = http.HandlerFunc(func(w http.ResponseWriter, r* http.Request) {
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, MaxRequestSize))
+
+	r.Body.Close()
+	r.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+
+	if err != nil {
+		JsonErrorHandler.ServeHTTP(w, r)
+	}
+
+	if len(body) == 0 {
+		return
+	}
+
+	var jsonBody interface{}
+	err = json.Unmarshal(body, &jsonBody)
+
+	if err != nil {
+		JsonErrorHandler.ServeHTTP(w, r)
+	}
+})
 
 // A set of middlewares for validating json in the request to a service
-var JsonRequestMiddlewares []http.Handler
+var JsonRequestMiddlewares = []http.Handler{
+	jsonValidator,
+}
 
 // A set of middlewares for validating json in the response from a service
-var JsonResponseMiddlewares []http.Handler
-
-func init() {
-	JsonErrorHandler = http.HandlerFunc(func(w http.ResponseWriter, r* http.Request) {
-		w.WriteHeader(500)
-	})
-
-	jsonValidator := http.HandlerFunc(func(w http.ResponseWriter, r* http.Request) {
-		body, err := ioutil.ReadAll(io.LimitReader(r.Body, MaxRequestSize))
-
-		r.Body.Close()
-		r.Body = ioutil.NopCloser(bytes.NewBuffer(body))
-
-		if err != nil {
-			JsonErrorHandler.ServeHTTP(w, r)
-		}
-
-		if len(body) == 0 {
-			return
-		}
-
-		var jsonBody interface{}
-		err = json.Unmarshal(body, &jsonBody)
-
-		if err != nil {
-			JsonErrorHandler.ServeHTTP(w, r)
-		}
-	})
-
-	JsonRequestMiddlewares = []http.Handler{
-		jsonValidator,
-	}
-
-	JsonResponseMiddlewares = []http.Handler{
-		jsonValidator,
-	}
+var JsonResponseMiddlewares = []http.Handler{
+	jsonValidator,
 }
